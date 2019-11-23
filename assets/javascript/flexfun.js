4e7cfc83e881629ba998963ad464c00a2082b215
unknown <mrhdigital@yahoo.com>
2019-11-22 20:57:33 -0600

    add transitionend event

--- assets/javascript/flexfun.js
+++ assets/javascript/flexfun.js
@@ -6,4 +6,13 @@ function toggleOpen() {
   console.log('Hello');
   this.classList.toggle('open');
 }
+
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes('flex')) {
+      this.classList.toggle('open-active');
+    }
+  }
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
