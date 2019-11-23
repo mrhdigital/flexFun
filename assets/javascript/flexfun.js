d8f315513cf7c53d4ebcaf1cb16d0bf078996cfe
unknown <mrhdigital@yahoo.com>
2019-11-22 20:53:04 -0600

    add click listner

--- assets/javascript/flexfun.js
+++ assets/javascript/flexfun.js
@@ -1,1 +1,9 @@
-alert("Hello");
+//alert("Hello");
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  console.log('Hello');
+  this.classList.toggle('open');
+}
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
